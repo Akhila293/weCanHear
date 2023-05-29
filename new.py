b781535517c1748d51187561b8724d3330dd4fef
Akhila293 <116826626+Akhila293@users.noreply.github.com>
2023-05-29 12:26:07 +0530

    Delete new.py

--- new.py
+++
@@ -1,8 +0,0 @@
-
-from gtts import gTTS
-import os
-readtext="Hello weCanHear, It is a great platform where you can listen the speech which is present in text, Thankyou for hearing."
-language = 'en'
-inst = gTTS(text=readtext, lang=language, slow=False)
-inst.save("myfile.mp3")
-os.system("myfile.mp3")
